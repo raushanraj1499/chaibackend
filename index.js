401b19832f13e2e9fd5b4fdecf3b39d32a994a16
Raushan Raj <raushanraj876540@gmail.com>
2025-06-24 14:10:25 +0530

    github data added

--- index.js
+++ index.js
@@ -2,7 +2,41 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000
-
+const gitdata={
+  "login": "raushanraj1499",
+  "id": 145265932,
+  "node_id": "U_kgDOCKiVDA",
+  "avatar_url": "https://avatars.githubusercontent.com/u/145265932?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/raushanraj1499",
+  "html_url": "https://github.com/raushanraj1499",
+  "followers_url": "https://api.github.com/users/raushanraj1499/followers",
+  "following_url": "https://api.github.com/users/raushanraj1499/following{/other_user}",
+  "gists_url": "https://api.github.com/users/raushanraj1499/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/raushanraj1499/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/raushanraj1499/subscriptions",
+  "organizations_url": "https://api.github.com/users/raushanraj1499/orgs",
+  "repos_url": "https://api.github.com/users/raushanraj1499/repos",
+  "events_url": "https://api.github.com/users/raushanraj1499/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/raushanraj1499/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Raushan",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 5,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2023-09-17T12:06:27Z",
+  "updated_at": "2025-05-13T06:39:46Z"
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -13,6 +47,10 @@ app.get('/twitter', (req, res)=>{
 
 app.get('/login', (req, res) => {
     res.send('<h1>Please Login at Chai aur Code</h1>')
+})
+
+app.get('/github', (req, res) => {
+    res.json(gitdata);
 })
 
 app.listen(process.env.PORT, () => {
